f8d82f6236e3556cda4ac14fbf96593a74e36b23
RaquelitaR <raquelita15@ru.is>
2016-10-28 16:13:30 +0000

    - Did some work on BW.java - Failed only one test in Mooshak Will fix it tonight or tomorrow

--- packet/s5/BurrowsWheeler.java
+++ packet/s5/BurrowsWheeler.java
@@ -4,6 +4,8 @@ import edu.princeton.cs.algs4.BinaryIn;
 import edu.princeton.cs.algs4.BinaryOut;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
 public class BurrowsWheeler {
 
 	// apply Burrows-Wheeler transform, reading from standard input and writing to standard output
@@ -11,6 +13,7 @@ public class BurrowsWheeler {
 		String input = in.readString();
 		CircularSuffixArray circularSuffixArray = new CircularSuffixArray(input);
 
+        // prints the index where input is in the circularSuffixArray
 		for (int i = 0; i < circularSuffixArray.length(); i++){
 			if(circularSuffixArray.index(i) == 0){
 				out.write(i);
@@ -18,20 +21,65 @@ public class BurrowsWheeler {
 			}
 		}
 
+		// the input and the last character in each row
+        // which is the first one in the previous rotation
+        // (that's why we use -1)
 		for(int i = 0; i < circularSuffixArray.length(); i++){
 			int index = circularSuffixArray.index(i);
 			if(index == 0){
 				out.write(input.charAt(input.length() - 1));
 			}else{
-				out.write(input.charAt(index));
+				out.write(input.charAt(index - 1));
 			}
 		}
 
 	}
-	
+
 	// apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
 	public static void inverseTransform(BinaryIn in, BinaryOut out) {
 
+	    // index where the original suffix appears in the shifted sorted suffixes
+		int first = in.readInt();
+        // reading the characters in the last column of each row
+		String input = in.readString();
+
+        // the characters in the last column of each row in array
+		char[] t = input.toCharArray();
+        // new array of indexes construct
+		int[] next = new int[t.length];
+        // first row is sorted t[]
+		char[] sorted = new char[t.length];
+
+        // copying and initializing the values
+		for (int i = 0; i < sorted.length; i++) {
+			sorted[i] = t[i];
+			next[i] = -1;
+		}
+		// sort the values
+		Arrays.sort(sorted);
+
+        // calculating the next array
+		for (int i = 0; i < sorted.length; i++) {
+			for (int j = 0; j < t.length; j++) {
+                // If sorted row i and j both start with the same character
+                // and i < j, then next[i] < next[j]
+				if (sorted[i] == t[j]) {
+					if (i > 0 && sorted[i - 1] == sorted[i] && next[i - 1] >= j) {
+						continue;
+					}
+					next[i] = j;
+					break;
+				}
+			}
+		}
+
+		// printing the calculated values
+		out.write(sorted[first]);
+		int current = next[first];
+		while (current != first) {
+			out.write(sorted[current]);
+			current = next[current];
+		}
 	}
 
 	public static void main(String[] args) {
